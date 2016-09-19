e0daa1d1b16e066f00c5d47f33edee3f7954322e
Kate Catanio <kmcatanio@gmail.com>
2016-09-19 18:46:37 -0500

    Functioning skeleton

--- main.py
+++ main.py
@@ -20,24 +20,45 @@ class Handler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
+	def render_blog(self, title="", body=""):
+		 blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5")
+
+		 self.render("blog.html", title = title, body = body, blogs = blogs)
+
 class Blog(db.Model):
 	title = db.StringProperty(required = True)
 	body = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
 
+class ViewPostHandler(Handler):
+	def get(self, id):
+		
+		entry_id = Blog.get_by_id(int(id))
+		
+		if entry_id:
+			self.render("blog.html", blogs = [entry_id])
+
+		else:
+			error="There is no post with this ID"
+			self.response.out.write(error)
+
+		
 # Main landing page (incomplete)
 
 class MainPage(Handler):
-	#def render_blog(self, title="", body=""):
-		#blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC")
-	def get(self):
-		self.render("blog.html")
 
-# Full Blog Archive (incomplete)
-
-class Blog(Handler):
+	
 	def get(self):
-		self.render("blog.html")
+		self.render_blog()
+		
+
+# Last 5 posts
+
+class BlogHandler(Handler):
+
+	def get(self):
+		self.render_blog()
+		
 
 # New Post Page (incomplete)
 
@@ -51,18 +72,27 @@ class NewPost(Handler):
 	def get(self):
 		self.render_newpost_form()
 
-	# Collects the inputs (where I left off)
+	# Collects the inputs and responds to interaction
 	def post(self):
 		title = self.request.get("title")
 		body = self.request.get("body")
 
 		if title and body:
-			self.write("Success!")
+		 	b = Blog(title = title, body = body)
+			b.put()
+
+			self.redirect("/blog/%s" % str(b.key().id()))
+
 		else:
 			error = "Please submit both a title and some blog content."
-			self.render_newpost_form(title, body, error = error)
+			self.render_newpost_form(title, body, error)
 
 
-app = webapp2.WSGIApplication([('/', MainPage),
-	('/blog', Blog),
-	('/newpost', NewPost)], debug=True)
+app = webapp2.WSGIApplication([
+	webapp2.Route('/blog/<id:\d+>', ViewPostHandler),
+	('/', MainPage),
+	('/blog', BlogHandler),
+	('/newpost', NewPost),
+	
+	], debug=True)
+	
